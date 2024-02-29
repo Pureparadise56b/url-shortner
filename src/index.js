acce9c46f8201a09ae9fb7340a2c26354cd029e5
PureParadise56b <toufique26ajay@gmail.com>
2024-02-29 16:21:03 +0530

    checking done

--- src/index.js
+++ src/index.js
@@ -1,29 +1,15 @@
-// import dotenv from 'dotenv'
-// dotenv.config({ path: './.env' })
+import dotenv from 'dotenv'
+dotenv.config({ path: './.env' })
 
-// import { connectDb } from './db/connection.db.js'
-// import { app } from './app.js'
+import { connectDb } from './db/connection.db.js'
+import { app } from './app.js'
 
-// const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000
 
-// connectDb()
-//   .then(() => {
-//     app.listen(port, () =>
-//       console.log(`\nServer Started at http://localhost:${port}`)
-//     )
-//   })
-//   .catch((error) => console.log('Server Listing Error :: ', error))
-
-import express from 'express'
-
-const app = express()
-
-app.get('/', (req, res) => {
-  res.status(200).json({
-    statusCode: 200,
-    success: true,
-    messgae: 'hello from home page',
+connectDb()
+  .then(() => {
+    app.listen(port, () =>
+      console.log(`\nServer Started at http://localhost:${port}`)
+    )
   })
-})
-
-app.listen(process.env.PORT, () => console.log('server started...'))
+  .catch((error) => console.log('Server Listing Error :: ', error))
